46cf8063ef9dc24aac3a9a9a8e1296364766649c
zn <2535708074@qq.com>
2020-12-08 08:54:09 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -15,6 +15,7 @@ $.ajaxPrefilter(function(options) {
 
     // 全局统一挂载 complete 回调函数 ？？
     options.complete = function(res) {
+        console.log(res);
         // console.log('执行了 complete 回调：')
         // console.log(res)
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
